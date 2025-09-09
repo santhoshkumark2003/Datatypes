d23ad956e1c5e043282535cbfc07e6d0b60984c7
santhoshkumark2003 <santhoshk13022003@gmail.com>
2025-09-09 22:10:02 +0530

    updated

---
+++ MRF/script.js
@@ -0,0 +1,18 @@
+/*
+//*Map exmple
+let num=[1,2,3,4,5,6,7,8];
+let dbl=num.map(x=>x*2);
+console.log(dbl);
+*/
+
+/*
+//*Reduce example
+let num=[1,2,3,4,5,6,7,8];
+let sum=num.reduce((total,crt)=>total+crt,0);
+console.log(sum);
+*/
+
+//*Filter example
+let num=[1,2,3,4,5,6,7,8];
+let even=num.filter(x=>x%2===0);
+console.log(even);
